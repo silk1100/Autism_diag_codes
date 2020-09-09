d729a8d9ad74d6803ed1298ef877b98523a37202
kal1000 <mtali003@louisville.edu>
2020-09-09 16:23:56 -0400

    editing update features function

--- prepare_data.py
+++ prepare_data.py
@@ -1,6 +1,39 @@
 import pandas as pd
 
 
+def modifyMedRange2MedPlusMinusRange(df):
+    """
+    Convert the data representation from 1 column median, and 1 column IQR into
+    1 column median-IQR, and 1 column median+IQR for each morphological feature for each brain region
+    :param df: pandas.DataFrame
+    :return updated_df: pandas.DataFrame
+    """
+    old_feats = df.columns.to_list()
+    updated_feats = list()
+    for feat in old_feats:
+        featL = feat.split('_')
+        morph = featL[-1]
+        if 'med' in morph:
+            morph = "medMIQR"
+        else:
+            morph = "medPIQR"
+        featL[-1] = morph
+        updated_feats.append('_'.join(featL))
+    # Fix for age, sex, labels
+    updated_feats[-1] = old_feats[-1]
+    updated_feats[-2] = old_feats[-2]
+    updated_feats[-3] = old_feats[-3]
+    updated_df = pd.DataFrame(None, index=df.index, columns=updated_feats)
+    for feat in updated_df.columns:
+        if 'age' in feat or 'sex' in feat or 'labels' in feat:
+            continue
+        morph, brainreg, _ = feat.split('_')
+        required_cols_from_old_df = ['_'.join([morph,brainreg,'_med']),
+                                     '_'.join([morph,brainreg,'_20-80range'])]
+        med, iqr = df[required_cols_from_old_df]
+        # updated_feats
+
+
 def get_left_right_hemisphere(features):
     """
     Based on the nomenclature of the csv files (aparc, a2009s), this function returns the
@@ -56,3 +89,12 @@ def get_csvfile_ready(fldr, testratio=0.2, random_seed=11111111):
     df_test_right = df_test[right_cols]
 
     return df_train_left, df_train_right, df_test_left, df_test_right
+
+
+def main():
+    df = pd.read_csv("D:\\PhD\\Data\\aparc\\all_data.csv", index_col=0)
+    modifyMedRange2MedPlusMinusRange(df)
+
+
+if __name__ == '__main__':
+    main()
